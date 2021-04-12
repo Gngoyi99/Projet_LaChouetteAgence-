ee81f29f74a58aef1d9728d99096606b22931baa
Gngoyi99 <73467477+Gngoyi99@users.noreply.github.com>
2021-04-12 13:16:41 +0200

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+            require("postcss-uncss")(
+                {
+                    html: [
+                        './index.html'
+                    ]
+                }
+            )
+    ]
+}
